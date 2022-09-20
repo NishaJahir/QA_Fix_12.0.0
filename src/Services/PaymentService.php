76a05da13a03207ce977effafdcfa45fa87dd9cf
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-09-20 18:44:25 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -909,7 +909,7 @@ class PaymentService
         }
         
         // Form the bank details for invoice payments
-        if(in_array($transactionData['paymentName'], ['novalnet_invoice', 'novalnet_prepayment', 'novalnet_guaranteed_invoice'])) {
+        if(in_array($transactionData['paymentName'], ['novalnet_invoice', 'novalnet_prepayment', 'novalnet_guaranteed_invoice']) && in_array($transactionData['tx_status'], ['PENDING', 'CONFIRMED'])) {
             $transactionComments .= PHP_EOL . $this->getBankDetailsInformation($transactionData);
         }
         
