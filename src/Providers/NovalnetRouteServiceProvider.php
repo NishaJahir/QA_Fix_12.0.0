5856691975c36cc5bfdf7cc8e9518944d254aeea
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-09-20 17:47:55 +0530

    Update NovalnetRouteServiceProvider.php

--- src/Providers/NovalnetRouteServiceProvider.php
+++ src/Providers/NovalnetRouteServiceProvider.php
@@ -36,5 +36,6 @@ class NovalnetRouteServiceProvider extends RouteServiceProvider
         $router->match(['post', 'get'], 'payment/novalnet/webhook', 'Novalnet\Controllers\WebhookController@processNnWebhook');
         $router->match(['post', 'get'], 'payment/novalnet/processPayment', 'Novalnet\Controllers\PaymentController@processPayment');
         $router->get('payment/novalnet/paymentResponse', 'Novalnet\Controllers\PaymentController@paymentResponse');
+        $router->get('payment/novalnet/directPaymentProcess', 'Novalnet\Controllers\PaymentController@directPaymentProcess');
     }
 }
