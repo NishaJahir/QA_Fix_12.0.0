c6524fd76ccd14bc8a81f8e1adff2dac6ad5f97c
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-09-27 19:14:11 +0530

    Update NovalnetGooglePayButtonDataProvider.php

--- src/Providers/DataProvider/NovalnetGooglePayButtonDataProvider.php
+++ src/Providers/DataProvider/NovalnetGooglePayButtonDataProvider.php
@@ -63,12 +63,12 @@ class NovalnetGooglePayButtonDataProvider
         // Required details for the Google Pay button
         $googlePayData = [
             'clientKey' => trim($settingsService->getNnPaymentSettingsValue('novalnet_client_key')),
-            'MerchantId' => $this->settingsService->getNnPaymentSettingsValue('payment_active', 'novalnet_google_pay'),
-            'sellerName' => $this->settingsService->getNnPaymentSettingsValue('business_name', 'novalnet_google_pay'),
-            'enforce' => $this->settingsService->getNnPaymentSettingsValue('enforce', 'novalnet_google_pay'),
-            'buttonType' => $this->settingsService->getNnPaymentSettingsValue('button_type', 'novalnet_google_pay'),
-            'buttonTheme' => $this->settingsService->getNnPaymentSettingsValue('button_theme', 'novalnet_google_pay'),
-            'buttonHeight' => $this->settingsService->getNnPaymentSettingsValue('button_height', 'novalnet_google_pay')
+            'MerchantId' => $settingsService->getNnPaymentSettingsValue('payment_active', 'novalnet_google_pay'),
+            'sellerName' => $settingsService->getNnPaymentSettingsValue('business_name', 'novalnet_google_pay'),
+            'enforce' => $settingsService->getNnPaymentSettingsValue('enforce', 'novalnet_google_pay'),
+            'buttonType' => $settingsService->getNnPaymentSettingsValue('button_type', 'novalnet_google_pay'),
+            'buttonTheme' => $settingsService->getNnPaymentSettingsValue('button_theme', 'novalnet_google_pay'),
+            'buttonHeight' => $settingsService->getNnPaymentSettingsValue('button_height', 'novalnet_google_pay')
            ];
         
         
